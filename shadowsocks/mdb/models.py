6e56cfc9aa0597a2553baf2002a60c3635382c63
ehco1996 <zh19960202@gmail.com>
2019-05-28 09:46:11 +0800

    add check_user_server

--- shadowsocks/mdb/models.py
+++ shadowsocks/mdb/models.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 import logging
+from collections import defaultdict
 
 import peewee as pw
 
@@ -44,7 +45,6 @@ class User(BaseModel, HttpSessionMixin):
 
     @classmethod
     def create_or_update_user_from_data(cls, data):
-        # TODO 用户更换端口/加密方式时
         for user_config in data:
             user_id = user_config.pop("user_id")
             # NOTE 兼容api
@@ -57,10 +57,10 @@ class User(BaseModel, HttpSessionMixin):
 
     @classmethod
     def init_user_servers(cls):
-        for user in cls.select().where(cls.enable == True):
+        for user in cls.select():
             us, _ = UserServer.get_or_create(user_id=user.user_id)
             loop = asyncio.get_event_loop()
-            loop.create_task(us.init_server())
+            loop.create_task(us.init_server(user))
 
     @classmethod
     def shutdown_user_servers(cls):
@@ -102,25 +102,53 @@ class User(BaseModel, HttpSessionMixin):
 
 class UserServer(BaseModel):
 
-    __running_servers__ = {}
+    __running_servers__ = defaultdict(dict)
 
     user_id = pw.IntegerField(primary_key=True)
 
-    async def init_server(self):
-        if self.user_id in self.__running_servers__:
-            return
+    @property
+    def tcp_server(self):
+        try:
+            return self.__running_servers__[self.user_id]["tcp"]
+        except KeyError:
+            return None
 
+    @property
+    def udp_server(self):
+        try:
+            return self.__running_servers__[self.user_id]["udp"]
+        except KeyError:
+            return None
+
+    @tcp_server.setter
+    def tcp_server(self, server):
+        if self.tcp_server:
+            self.tcp_server.close()
+        self.__running_servers__[self.user_id]["tcp"] = server
+
+    @udp_server.setter
+    def udp_server(self, server):
+        if self.udp_server:
+            self.udp_server.close()
+        self.__running_servers__[self.user_id]["udp"] = server
+
+    @property
+    def is_running(self):
+        return self.user_id in self.__running_servers__
+
+    async def init_server(self, user):
+        self.is_running and self.check_user_server(user)
+
+        if self.is_running or not user.enable:
+            return
         loop = asyncio.get_event_loop()
         try:
-            user = User.get_by_id(self.user_id)
             tcp_server = await loop.create_server(LocalTCP(user), user.host, user.port)
             udp_server, _ = await loop.create_datagram_endpoint(
                 LocalUDP(user), (user.host, user.port)
             )
-            self.__running_servers__[self.user_id] = {
-                "tcp": tcp_server,
-                "udp": udp_server,
-            }
+            self.tcp_server = tcp_server
+            self.udp_server = udp_server
             logging.info(
                 "user_id:{} method:{} password:{} port:{} 已启动".format(
                     user.user_id, user.method, user.password, user.port
@@ -129,14 +157,24 @@ class UserServer(BaseModel):
         except OSError as e:
             logging.warning(e)
 
+    def check_user_server(self, user):
+        # get running server user
+        tcp_user = self.tcp_server._protocol_factory.user
+        if (
+            not user.enable
+            or tcp_user.port != user.port
+            or tcp_user.password != user.password
+        ):
+            self.close_server()
+
     def close_server(self):
         self.delete_instance()
 
         if self.user_id not in self.__running_servers__:
             return
 
+        user = self.tcp_server._protocol_factory.user
         server_data = self.__running_servers__.pop(self.user_id)
         server_data["tcp"].close()
         server_data["udp"].close()
-        user = User.get_by_id(self.user_id)
-        logging.info(f"user_id:{user} prot:{user.port} 已关闭!")
+        logging.info(f"user_id:{user} port:{user.port} 已关闭!")
